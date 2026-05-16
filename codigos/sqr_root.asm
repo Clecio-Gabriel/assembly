2d8dcf59ad79543dc6bd65d93e2533bb707d8814
Clecio-Gabriel <cleciogabofelix@gmail.com>
2026-05-15 22:21:40 -0300

    I MANAGED TO INPUT AN INTEGER AND PRINT A STRING(IM HAPPY)

--- codigos/sqr_root.asm
+++ codigos/sqr_root.asm
@@ -5,7 +5,14 @@ str2:	.asciiz ": raiz "
 	.globl main
 
 main:
-	li $s0, 0	# input
+	li $v0, 4	#To print a string
+	la $a0, str1	#Load the str1 adress
+	syscall 	#call the system
+	
+	li $s0, 0	# input variable
+	li $v0, 5	# to read an integer
+	syscall 
+	move $s0, $v0
 	
 	li $s1, 1	# R <= 1
 	li $s2, 2	# D <= 2
@@ -13,12 +20,18 @@ main:
 	j loopSquare
 	
 loopSquare:
-	blt $s0, $s3, end	#if input < S, end the loop
+	blt $s0, $s3, check	#if input < S, end the loop
+	
 	addi $s1, $s1, 1	# R += 1
 	addi $s2, $s2, 2	# D += 2
 	add $s3, $s3, $s2	# S <= S + D
 	addi $s3, $s3, 1	# S += 1
+	
 	j loopSquare
+
+check:
+	
+	j end
 	
 end:	
 	
